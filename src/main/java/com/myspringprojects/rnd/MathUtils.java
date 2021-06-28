ad9a637a22c2577127dad80e8b39c970f3a374d8
P2000948290 <Hareesh.Ravindran@in.ey.com>
2021-06-28 15:25:14 +0530

    Created The JUnit5 test project.

---
+++ src/main/java/com/myspringprojects/rnd/MathUtils.java
@@ -0,0 +1,9 @@
+package com.myspringprojects.rnd;
+
+public class MathUtils {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+}
---
+++ src/test/java/com/myspringprojects/rnd/MathUtilsTest.java
@@ -0,0 +1,13 @@
+package com.myspringprojects.rnd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathUtilsTest {
+
+    @Test
+    void add() {
+        System.out.println("This test ran!!");
+    }
+}
