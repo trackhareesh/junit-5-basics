7808795dc36251e7f55e2518e75770db91be863a
P2000948290 <Hareesh.Ravindran@in.ey.com>
2021-06-28 17:03:59 +0530

    Tested Exception Scenario using assertThrows. Tested the @BeforeEach, @AfterAll lifecycle methods.

--- src/test/java/com/myspringprojects/rnd/MathUtilsTest.java
+++ src/test/java/com/myspringprojects/rnd/MathUtilsTest.java
@@ -1,14 +1,28 @@
 package com.myspringprojects.rnd;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MathUtilsTest {
 
+    private MathUtils mathUtils;
+
+    @BeforeEach
+    void setup() {
+        mathUtils = new MathUtils();
+    }
+
+    @AfterAll
+    static void cleanup() {
+        System.out.println("Cleaning Up");
+    }
+
     @Test
     void testAdd() {
-        MathUtils mathUtils = new MathUtils();
         int expected = 2;
         int actual = mathUtils.add(1, 1);
         assertEquals(expected, actual, "The add method should add 2 numbers");
@@ -18,11 +32,18 @@ class MathUtilsTest {
 
     @Test
     void testComputeCircleArea() {
-
-        MathUtils mathUtils = new MathUtils();
         double radius = 10.0;
         double expected = Math.PI * radius * radius;
         double actual = mathUtils.computeCircleArea(radius);
         assertEquals(expected, actual, "Area of the circle should be pi * radius * radius");
+    }
+
+    @Test
+    void testDivideWithZero() {
+        assertThrows(
+                ArithmeticException.class,
+                () -> mathUtils.divide(1, 0), "" +
+                        "Division by zero should throw ArithmeticException"
+        );
     }
 }
