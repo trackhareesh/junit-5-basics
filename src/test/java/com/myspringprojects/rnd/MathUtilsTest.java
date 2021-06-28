02f277514a240153dd290af5d01c389e5fc81c19
P2000948290 <Hareesh.Ravindran@in.ey.com>
2021-06-28 16:33:21 +0530

    Created 2 methods by following TDD. Added the surefire plugin to pom for running from command line.

--- src/test/java/com/myspringprojects/rnd/MathUtilsTest.java
+++ src/test/java/com/myspringprojects/rnd/MathUtilsTest.java
@@ -7,7 +7,22 @@ import static org.junit.jupiter.api.Assertions.*;
 class MathUtilsTest {
 
     @Test
-    void add() {
-        System.out.println("This test ran!!");
+    void testAdd() {
+        MathUtils mathUtils = new MathUtils();
+        int expected = 2;
+        int actual = mathUtils.add(1, 1);
+        assertEquals(expected, actual, "The add method should add 2 numbers");
+
+    }
+
+
+    @Test
+    void testComputeCircleArea() {
+
+        MathUtils mathUtils = new MathUtils();
+        double radius = 10.0;
+        double expected = Math.PI * radius * radius;
+        double actual = mathUtils.computeCircleArea(radius);
+        assertEquals(expected, actual, "Area of the circle should be pi * radius * radius");
     }
 }
